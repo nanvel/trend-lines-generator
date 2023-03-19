3263b97cf8a3afe9b08f7d75e030dbbe87586a14
Oleksandr Polieno <polyenoom@gmail.com>
2023-03-19 17:44:35 +0700

    remove data fixture

--- tests/factories/test_board.py
+++ tests/factories/test_board.py
@@ -1,9 +1,27 @@
+from pandas import DataFrame
+
 from tlines.factories import BoardFactory
 from tlines.models import Side
 
 
-def test_from_series(data):
-    df = data.load_df(path="example.csv")
+def test_from_series():
+    df = (
+        DataFrame(
+            {
+                "ts": [
+                    "2023-03-04 23:05:00",
+                    "2023-03-04 23:07:00",
+                    "2023-03-04 23:08:00",
+                    "2023-03-04 23:09:00",
+                    "2023-03-04 23:10:00",
+                ],
+                "high": [0.07405, 0.07404, 0.07408, 0.07408, 0.07414],
+                "low": [0.07401, 0.07403, 0.07394, 0.07399, 0.07408],
+            }
+        )
+        .astype({"ts": "datetime64[s]"})
+        .set_index("ts")
+    )
 
     board = BoardFactory().from_series(low_series=df["low"], high_series=df["high"])
 
