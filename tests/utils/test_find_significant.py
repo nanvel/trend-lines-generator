1b28d2964e4bc1d8032ac97cef33c91bcf613c10
Oleksandr Polieno <polyenoom@gmail.com>
2023-04-01 16:21:50 +0700

    Use mark.parametrize

--- tests/utils/test_find_significant.py
+++ tests/utils/test_find_significant.py
@@ -1,8 +1,11 @@
+import pytest
+
 from trend_lines.utils import find_significant
 
 
-def test_find_significant():
-    test_case = [
+@pytest.mark.parametrize(
+    "start,stop,result",
+    [
         (10, 100, [10.0, 100.0]),
         (10, 126, [10.0, 100.0]),
         (0.065, 0.1, [0.1]),
@@ -12,7 +15,7 @@ def test_find_significant():
         (1.05, 2.55, [2.0, 2.5]),
         (18000, 28000, [20000, 25000]),
         (1, 100, [1, 10, 100]),
-    ]
-
-    for start, stop, result in test_case:
-        assert find_significant(start=start, stop=stop) == result
+    ],
+)
+def test_find_significant(start, stop, result):
+    assert find_significant(start=start, stop=stop) == result
